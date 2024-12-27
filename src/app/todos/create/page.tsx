d6fc577ae499569d17a6ea8407a93f11888b1dbf
Natalia Yakovleva <yakovlevane29@gmail.com>
2024-12-27 16:24:35 +0200

    added type Todo and ui.shadcn for components

--- src/app/todos/create/page.tsx
+++ src/app/todos/create/page.tsx
@@ -1,9 +1,13 @@
-
+import { Button } from "@/components/ui/button"
 
 export default function CreateTodo() {
   return (
     <section>
       <h2>CreateTodo</h2>
+
+      <div>
+      <Button>Click me</Button>
+    </div>
     </section>
   )
 }
---
+++ src/types/todo.ts
@@ -0,0 +1,8 @@
+export type Todo = {
+  id: string;
+  title: string;
+  due_date: string;
+  description: string;
+  priority: string;
+  completed: boolean;
+};
