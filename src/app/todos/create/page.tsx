4ea4c2844d2157253fdfa8c8aff4637bd8bd77b5
Natalia Yakovleva <yakovlevane29@gmail.com>
2024-12-29 22:22:04 +0200

    tried to add supabase-had mistake

--- src/app/todos/create/page.tsx
+++ src/app/todos/create/page.tsx
@@ -1,13 +1,47 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
+import { Todo } from "@/types/todo";
 
-export default function CreateTodo() {
+export default async function CreateTodo() {
+  const cookieStore = await cookies();
+  const supabase = createClient(cookieStore);
+
+
+
+  const { data: todos, error} = await supabase.from('todos').select();
+
+  if (error) {
+    throw new Error(`Failed to fetch todos: ${error.message}`);
+  }
+
+  console.log(todos);
+
   return (
     <section>
       <h2>CreateTodo</h2>
 
       <div>
-      <Button>Click me</Button>
-    </div>
+        <Button>Click me</Button>
+      </div>
+
+      <div>
+        <h3>UL</h3>
+        {/* <ul>
+          {todos?.map((todo:any) => (
+            <li>{todo}</li>
+          ))}
+        </ul> */}
+      </div>
     </section>
-  )
+  );
 }
+
+
+
+
+
+
+
+  
+
