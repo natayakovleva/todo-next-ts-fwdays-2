c38b933bad4f9914779044af98bd72b45692c267
Natalia Yakovleva <yakovlevane29@gmail.com>
2024-12-30 11:09:16 +0200

    added addTodos.ts

---
+++ src/app/todos/actions/addTodos.ts
@@ -0,0 +1,31 @@
+"use server";
+
+import { cookies } from "next/headers";
+import { createClient } from "@/utils/supabase/server";
+import { Priority } from "@/constants/todos";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+
+export default async function addTodo(formData: FormData) {
+  const cookieStore = cookies();
+  const supabase = createClient(cookieStore);
+
+  const todoData = {
+    title: formData.get('title')?.toString() || "",
+    description: formData.get('description')?.toString() || "",
+    due_date: formData.get('due_date')? 
+              new Date((formData.get('due_date') as object).toString())
+              : null,
+    priority: formData.get('priority')?.toString() || Priority.P4,
+    completed: Boolean(formData.get('completed'))
+  }
+
+  const { error } = await supabase.from('todos').insert([todoData]);
+
+  if (error) {
+    throw new Error(`Failed to insert todo: ${error.message}`);
+  }
+
+  revalidatePath('/todos');
+  redirect('/todos');
+}
